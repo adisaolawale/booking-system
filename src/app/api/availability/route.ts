4ea23deb7ae2357da876ab5eb6b82d66b62221b4
adisaolawale <adisaolawale10@gmail.com>
2026-08-13 16:10:19 +0100

    ready to launch 10

--- src/app/api/availability/route.ts
+++ src/app/api/availability/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import { getAvailableSlots } from "@/services/availability.logic";
+// import { NextResponse } from "next/server";
+// import { getAvailableSlots } from "@/services/availability.logic";
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
+// export async function GET(req: Request) {
+//   const { searchParams } = new URL(req.url);
 
-  const businessId = searchParams.get("businessId")!;
-  const date = searchParams.get("date")!;
-  const duration = Number(searchParams.get("duration"));
+//   const businessId = searchParams.get("businessId")!;
+//   const date = searchParams.get("date")!;
+//   const duration = Number(searchParams.get("duration"));
 
-  const slots = await getAvailableSlots({
-    businessId,
-    date,
-    serviceDuration: duration,
-  });
+//   const slots = await getAvailableSlots({
+//     businessId,
+//     date,
+//     serviceDuration: duration,
+//   });
 
-  return NextResponse.json(slots);
-}
+//   return NextResponse.json(slots);
+// }
